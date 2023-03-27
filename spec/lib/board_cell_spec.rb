2cad51d21929bf332893ad87583aaf2eb2dbfcc3
Hugo Hernani <hhernanni@gmail.com>
2023-03-27 00:27:34 -0300

    Add BoardCell object to handle intermediary actions

--- spec/lib/board_cell_spec.rb
+++ spec/lib/board_cell_spec.rb
@@ -1,16 +1,23 @@
 describe BoardCell do
   subject(:board_cell) { described_class.new(board) }
 
-  let(:board) { Board.new(size: size, starts_with: 1) }
+  let(:board) { Board.new(size: 3) }
 
   describe '#cell_for' do
+    subject(:cell_for) { board_cell.cell_for(spot) }
+    let(:spot) { 2 }
+    
     context 'when given spot is taken' do
-      let(:spot) { 2 }
-
       before { board[spot] = 'X' }
 
       it 'gives a null value' do
-        expect(board.cell_for(spot)).to eq nil
+        is_expected.to be nil
+      end
+    end
+
+    context 'when given spot is not taken' do
+      it 'gives found Slot object back' do
+        is_expected.to be_a(Slot)
       end
     end
   end
